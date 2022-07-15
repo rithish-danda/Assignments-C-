29ba64a7594161e53955ec9ac4dccff6c392eb51
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-07-15 11:11:15 +0530

    Create 04.c

---
+++ Asssignment-1/04.c
@@ -0,0 +1,11 @@
+// 04
+#include<stdio.h>
+int main()
+{
+    float r,a;
+    printf("Input the value of radius:");
+    scanf("%f",&r);
+    a = 2*3.14*r;
+    printf("Area of circle is %g having the radius %g.",a,r);
+    return 0;
+}
