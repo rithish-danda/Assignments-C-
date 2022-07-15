ba786db7e768eb3c33f9dd916d5e54dbb18af36e
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-07-15 22:37:12 +0530

    Create 2-01.c

---
+++ Asssignment-2/2-01.c
@@ -0,0 +1,11 @@
+// 01
+#include<stdio.h>
+int main()
+{
+    int n,l;
+    printf("Input a number: ");
+    scanf("%d",&n);
+    l = n%10;
+    printf("The digit in units place is %d",l);
+    return 0;
+}
