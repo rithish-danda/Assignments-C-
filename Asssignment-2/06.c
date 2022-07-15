007a1a179f6b9b337d11358fc900ee3cde0ff6ff
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-07-15 23:13:50 +0530

    Create 06.c

---
+++ Asssignment-2/06.c
@@ -0,0 +1,11 @@
+// 06
+#include<stdio.h>
+int main()
+{
+    char ch;
+    int v;
+    printf("Input any character: ");
+    scanf("%c",&ch);
+    printf("The ASCII value of %c is %d",ch,ch);
+    return 0;
+}
