5563f758e0d8dd389b007687103eb7215c90bc91
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-07-15 11:11:45 +0530

    Create 05.c

---
+++ Asssignment-1/05.c
@@ -0,0 +1,11 @@
+// 05 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[10];
+    scanf("%s",s);
+    int n = printf("%s",s);
+    printf("\nThe length of the string is %d",n);
+    return 0;
+}
