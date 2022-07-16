d606d970d7b902afce81d49f44e29fddfa3ce5e7
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-07-16 17:47:41 +0530

    Create 09.c

---
+++ Asssignment-2/09.c
@@ -0,0 +1,12 @@
+// 09
+#include<stdio.h>
+int main()
+{
+    int i,f,c,d;
+    i = sizeof(int);
+    f = sizeof(float);
+    c = sizeof(char);
+    d = sizeof(double);
+    printf("size of int = %d\nsize of float = %d\nsize of char = %d\nsize of double = %d",i,f,c,d);
+    return 0;
+}
