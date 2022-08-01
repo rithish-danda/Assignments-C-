071eef6f7a3b86a4ba32efebafe8e8a765e15782
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-01 16:15:09 +0530

    a05 02

---
+++ Assignment-5/2.c
@@ -0,0 +1,14 @@
+// a05 02
+#include<stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    while(i<=n)
+        {
+            printf("%d\n",i);
+            i++;
+        }
+    return 0;
+}
