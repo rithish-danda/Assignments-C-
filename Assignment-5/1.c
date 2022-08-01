60518a5063e316521b50995190fbfa4276839d79
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-01 16:14:10 +0530

    Create 1.c

---
+++ Assignment-5/1.c
@@ -0,0 +1,14 @@
+//a05 01
+#include<stdio.h>
+int main()
+{
+    int n,i=0;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    while(i<n)
+        {
+            printf("MySirG\n");
+            i++;
+        }
+    return 0;
+}
