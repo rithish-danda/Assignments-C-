cb065e3aa00f679112bd5cf198a49d3812ec99ce
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-01 16:20:39 +0530

    Create 5.c

---
+++ Assignment-5/5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n=10,i=1,x;
+    printf("Enter the value of n: ");
+    scanf("%d",&x);
+    while(i<=10)
+        {
+            if(n%2!=0)
+                printf("%d\n",n);
+            i++;
+            n=n-1;
+        }
+    return 0;
+}
