3cfa09c4eafd151b18f5166429da2832e3cc54a0
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-01 16:11:12 +0530

    a04 03

---
+++ Assignment-4/3.c
@@ -0,0 +1,13 @@
+//a04 03
+#include<stdio.h>
+int main()
+{
+    int n=10,i=1;
+    while(i<=10)
+        {
+            printf("%d\n",n);
+            i++;
+            n=n-1;
+        }
+    return 0;
+}
