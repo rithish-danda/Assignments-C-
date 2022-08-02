27ac3f8a0764605da8f4b98d3f18510a0dc3a9fe
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-02 20:58:29 +0530

    a04 05

--- Assignment-4/5.c
+++ Assignment-4/5.c
@@ -1,14 +1,20 @@
 // a04 05
 #include<stdio.h>
+void printOdd(int n)
+{
+    int x=1;
+    for (int i=1;x<=n;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d\n",2*n-i);
+            x++;
+        }
+    }
+    return;
+}
 int main()
 {
-    int n=10,i=1;
-    while(i<=10)
-        {
-            if(n%2!=0)
-                printf("%d\n",n);
-            i++;
-            n=n-1;
-        }
+    printOdd(10);
     return 0;
 }
