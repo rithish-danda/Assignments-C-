863fa4a6ca90647c093373a22e01dda1eec59ee3
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-02 20:27:51 +0530

    a05 04

--- Assignment-5/4.c
+++ Assignment-5/4.c
@@ -1,15 +1,23 @@
 // a05 04
 #include<stdio.h>
+void printOdd(int n)
+{
+    int x=1;
+    for (int i=1;x<=n;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d\n",i);
+            x++;
+        }
+    }
+    return;
+}
 int main()
 {
-    int n,i=1;
+    int n;
     printf("Enter the value of n: ");
     scanf("%d",&n);
-    while(i<=n)
-        {
-            if(i%2!=0)
-                printf("%d\n",i);
-            i++;
-        }
+    printOdd(n);
     return 0;
 }
