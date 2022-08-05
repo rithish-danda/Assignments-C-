afccd1a4b375920ff082928edce5bd896b205d81
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-05 16:19:19 +0530

    a03 08

---
+++ Asssignment-3/8.c
@@ -0,0 +1,13 @@
+// a03 08
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("Enter the year: ");
+    scanf("%d",&year);
+    if(((year%4==0) && (year%100!=0)) || (year%400==0))
+        printf("Leap Year");
+    else
+        printf("Not a leap year");
+    return 0;
+}
