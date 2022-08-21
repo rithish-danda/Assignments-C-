af00d7e49c4aac8e42fb9de45e33e97eba843739
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-21 21:42:56 +0530

    a23 05

---
+++ Assignment-23/5.cpp
@@ -0,0 +1,10 @@
+// a23 05
+#include<iostream>
+using namespace std;
+int main(){
+    float l,b,h;
+    cout << "Input the length, breadth and height of the cuboid: ";
+    cin >> l >> b >> h;
+    cout << "The volume of the cuboids with given dimensions is " << l*b*h;
+    return 0;
+}
