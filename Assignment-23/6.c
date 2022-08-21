af94c70aeb593358803c5d361710380e13eebf5c
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-21 21:45:11 +0530

    a23 06

---
+++ Assignment-23/6.c
@@ -0,0 +1,11 @@
+// a23 06
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b,c, avg;
+    cout << "Input any three numbers: ";
+    cin >> a >> b >> c;
+    avg = (a+b+c)/3;
+    cout << "The average of " << a << ", " << b << ", " << c << " is " << avg;
+    return 0;
+}
