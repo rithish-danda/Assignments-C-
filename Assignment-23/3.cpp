fa5bbe56439f4d68156d3435dd6fa5e656cd16f4
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-21 21:23:38 +0530

    a23 03

---
+++ Assignment-23/3.cpp
@@ -0,0 +1,11 @@
+// a23 03
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout << "Input any two numbers: ";
+    cin >> a >> b;
+    c = a+b;
+    cout << "Sum of " << a << " and " << b << " is " << c;
+    return 0;
+}
