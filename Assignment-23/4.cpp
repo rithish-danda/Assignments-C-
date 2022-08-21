dd09ae5b124aff6e74bce228018d5c6a46f788d7
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-21 21:25:48 +0530

    a23 04

---
+++ Assignment-23/4.cpp
@@ -0,0 +1,11 @@
+// a23 04
+#include<iostream>
+using namespace std;
+int main(){
+    float r,a;
+    cout << "Input the radius: ";
+    cin >> r;
+    a = 3.14*r*r;
+    cout << "Area of circle with radius " << r << " is " << a;
+    return 0;
+}
