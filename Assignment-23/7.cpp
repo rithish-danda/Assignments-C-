b41f6a32ec0b9ed34469a37ccc2e2f161c50acf3
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-21 21:46:40 +0530

    a23 07

---
+++ Assignment-23/7.cpp
@@ -0,0 +1,11 @@
+// a23 07
+#include<iostream>
+using namespace std;
+int main(){
+    float n,s;
+    cout << "Input any number: ";
+    cin >> n;
+    s = n*n;
+    cout << "The square of " << n << " is " << s;
+    return 0;
+}
