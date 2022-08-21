fc2219fac13a7854c8dc3c1c09f15da36d3535d7
Rithish Danda <96982556+rithish-danda@users.noreply.github.com>
2022-08-21 21:52:33 +0530

    a23 09

---
+++ Assignment-23/9.cpp
@@ -0,0 +1,13 @@
+// a23 09
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "Input any two variables: ";
+    cin >> a >> b;
+    if ( a > b )
+        cout << a << " is greater than " << b;
+    else
+        cout << b << " is greater than " << a;
+    return 0;
+ }
